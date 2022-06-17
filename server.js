fd8bf2ce40cc2654f50235a42344d0df75b56132
Brian Harsha <harshabc4@gmail.com>
2022-06-17 17:56:23 -0400

    add bootstrap

--- server.js
+++ server.js
@@ -16,7 +16,9 @@ MongoClient.connect(dbConnectionStr, { useUnifiedTopology: true })
     })
     
 app.set('view engine', 'ejs')
-app.use(express.static('public'))
+// app.use(express.static('public'))
+// app.use(express.static(__dirname + '/node_modules/bootstrap/dist'));
+app.use(express.static(__dirname +''))
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
 
